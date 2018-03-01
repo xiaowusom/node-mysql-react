c991bacc4602feaf6912a7e31cc55d42a81daa87
伍健 <wujian@wujiandeMacBook-Air.local>
2018-03-01 14:16:21 +0800

    优化

--- xiaowu/routes/index.js
+++ xiaowu/routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const Sequelize = require('sequelize');
 var mysql = require('mysql');
-/* GET home page. */
 var connection = mysql.createConnection({
 host     : 'localhost',
 user     : 'root',
@@ -28,14 +27,15 @@ router.get('/', function(req, res, next) {
  	var id=req.body.id;
  	var name=req.body.name;
  	var imgUrl=req.body.imgUrl;
-  for(var i = 0; i <arr.length; i++){
+  var userName = null;
+  for(var i = 0; i < arr.length; i++){
+    console.log(name);
+    console.log(arr[i].name);
     if(name===arr[i].name){
-      var userName = name;
-      console.log(userName);
+      userName = name; 
     }
   }  
-    if(userName){
-      console.log("我执行了更新语句");
+  if(userName){
       connection.query("update pets set imgUrl='"+ imgUrl + "' where name='"+ userName + "'" ,function(err,rows,fields){
         console.log("update pets set imgUrl='"+ imgUrl + "' where name='"+ userName + "'" );
           connection.query('select * from pets',function(err,rows,fields){
@@ -44,20 +44,15 @@ router.get('/', function(req, res, next) {
          });     
        });  
     }else{
-          console.log(name);
-          console.log(arr[i].name);
-          console.log("我执行了插入语句");
       var  addSql = 'insert into pets(id, name, imgUrl) values("'+ id + '","'+ name + '","'+ imgUrl +'")'
       connection.query(addSql,function (err,rows, result) {
       connection.query('select * from pets',function(err,rows,fields){
       if(err) throw err;
       arr = rows; 
-});
-  
-});
+        });
+      });
     }
 });
-
 // 删除用户
 router.post("/del",function(req,res){
   var id = req.body.id;
@@ -74,10 +69,7 @@ router.post("/del",function(req,res){
   if(err) throw err;
   arr = rows;
   // console.log(arr);
-});
+    });
   })
 })
-
-
-
 module.exports = router;
